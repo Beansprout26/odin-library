c80ee8a4e52cbe10857921203bb5ad29962c8a0b
Beansprout26 <107769774+Beansprout26@users.noreply.github.com>
2024-07-22 23:02:37 -0600

    JavaScript
    added event listener for click event to #add-book-icon

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('cat');
+const myLibrary = [];
+
+function Book(){
+
+}
+
+document.getElementById('add-book-icon').addEventListener('click', addBookToLibrary)
+
+function addBookToLibrary(){
+    alert("button clicked!")
+}
