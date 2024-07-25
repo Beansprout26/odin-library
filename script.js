e703ae6f5402f89506eeef935f32eb974f762ada
Beansprout26 <107769774+Beansprout26@users.noreply.github.com>
2024-07-24 20:03:08 -0600

    JS
    added js to add book library array of new books to the console.
    
    TODO:
    render html/css when a new book is added

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 const myLibrary = [];
 
-const myBook = {
-    bookTitle: '',
 
-}
-
+//this is my book object
 function Book(title, author, pages, read){
-
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
+
+//this selects the input in the form and the value that will be the output
+document.getElementById('submitBtn').addEventListener('submit', addBookToLibrary);
+
+function addBookToLibrary(Book){
+    let title = document.getElementById('bookTitle').value;
+    let author = document.getElementById('bookAuthor').value;
+    let pages = document.getElementById('bookPages').value;
+    let read = document.getElementById('read').checked;
+    let newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+    console.log(myLibrary);
+    
+}
+
 
 //shows the form when the add book icon is clicked//
 document.getElementById('add-book-icon').addEventListener('click', openForm);
@@ -21,3 +36,8 @@ function openForm() {
     }
 }
 
+
+document.querySelector('.newBookForm').addEventListener('submit', function(event){
+    event.preventDefault();
+    addBookToLibrary(Book);
+})
